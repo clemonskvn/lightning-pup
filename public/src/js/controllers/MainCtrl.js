97b106c0114ec5c0729a87e38da8a989b5687c7c
Kevin Clemons <clemonskvn@gmail.com>
2016-04-10 20:49:43 -0500

    starting to add api to MainCtrl

--- public/src/js/controllers/MainCtrl.js
+++ public/src/js/controllers/MainCtrl.js
@@ -2,5 +2,17 @@
 angular.module('MainCtrl', []).controller('MainController', ['$scope', function($scope) {
 
 	$scope.tagline = 'To the moon and back!';
+    $scope.user = 'test';
+    $scope.pass = '123';
+    
+    $("submit").click(function() {
+        $.post("http://localhost:8080/api/user", {user: $scope.user, password: $scope.pass}, function(data) {
+            if(data === 'done')
+                {
+                    alert("login success");
+                }
+        });
+    });
+    
 
 }]);
