4405e43036501ae65f4b115ce65dafc95592f818
Kevin Clemons <clemonskvn@gmail.com>
2016-04-14 14:38:18 -0500

    adding validations to newuserform

--- public/src/js/controllers/UserDetailCtrl.js
+++ public/src/js/controllers/UserDetailCtrl.js
@@ -3,9 +3,51 @@ angular.module('UserDetailCtrl', []).controller('UserDetailController', ['$scope
     
     $scope.formData = {};
     
+    $scope.checkValidity = function(username) {
+        return UserDetail.checkValidity(username);
+    };
+    
     UserDetail.get().then(function(userlist) {
         $scope.users = userlist;
     });
+    
+    $scope.checkPassword = function() {
+        if ($scope.formData.password === $scope.formData.validate_password) {
+            $scope.newUserForm.validate_password.$setValidity("mismatchPassword", true);
+        } else {
+            $scope.newUserForm.validate_password.$setValidity("mismatchPassword", false);
+        }
+    };
+    
+    $scope.resetPasswordValidity = function() {
+        $scope.newUserForm.password.$setValidity("mismatchPassword", true);
+        $scope.newUserForm.password.$setValidity("passwordInvalidLength", true);
+        $scope.newUserForm.password.$setValidity("passwordInvalidUpper", true);
+        $scope.newUserForm.password.$setValidity("passwordInvalidLower", true);
+        $scope.newUserForm.password.$setValidity("passwordInvalidNumeric", true);
+        $scope.newUserForm.password.$setValidity("passwordInvalidNonalpha", true);
+    };
+    
+    $scope.phoneTypeOptions = [{
+        id: 1,
+        label: 'MOBILE'
+    },{
+        id: 2,
+        label: 'WORK'
+    },{
+        id: 3,
+        label: 'HOME'
+    },{
+        id: 4,
+        label: 'FAX'
+    },{
+        id: 5,
+        label: 'PAGER'
+    },{
+        id: 6,
+        label: 'OTHER'
+    }];
+    $scope.formData.phone_type = $scope.phoneTypeOptions[0];
     
     $scope.submit = function(data) {
         UserDetail.create(data).then(function(msg) {
@@ -13,9 +55,5 @@ angular.module('UserDetailCtrl', []).controller('UserDetailController', ['$scope
             $scope.message = msg;
             //$window.location.href='/profile';
         });
-    };
-    
-    $scope.checkValidity = function(username) {
-        return UserDetal.checkValidity(username);
     };
 }]);
--- public/src/js/directives/lpValidateDirectives.js
+++ public/src/js/directives/lpValidateDirectives.js
@@ -1,7 +1,6 @@
 // public/js/directives/lpValidateDirectives.js
 angular.module('lpValidateDirectives', [])
     .directive('lpValidateUsername', function() {
-        // username must start with a letter and may only contain letters, numbers, underscores, periods, and single quotes
         var username_regex = /^[a-z][a-z0-9_.\']*$/i;
         return {
             restrict: 'A',
@@ -9,9 +8,54 @@ angular.module('lpValidateDirectives', [])
             link: function(scope, elm, attrs, ctrl) {
                 ctrl.$validators.lpValidateUsername = function(modelValue, viewValue) {
                     if(username_regex.test(viewValue)) {
+                        ctrl.$setValidity("usernameInvalidFormat", true);
                         return true;
                     }
+                    ctrl.$setValidity("usernameExists", true);
+                    ctrl.$setValidity("usernameInvalidFormat", false);
                     return false;
+                };
+            }
+        };
+    })
+
+    .directive('lpValidatePassword', function() {
+        var uppercase_regex = /[A-Z]/,
+            lowercase_regex = /[a-z]/,
+            numeric_regex = /\d/,
+            nonalpha_regex = /\W/;
+        return {
+            restrict: 'A',
+            require: 'ngModel',
+            link: function(scope, elm, attrs, ctrl) {
+                ctrl.$validators.lpValidatePassword = function(modelValue, viewValue) {
+                    if(viewValue === undefined) {
+                        scope.resetPasswordValidity();
+                        ctrl.$setValidity("passwordInvalidLength", false);
+                        return false;
+                    } else if(viewValue.length < 8) {
+                        scope.resetPasswordValidity();
+                        ctrl.$setValidity("passwordInvalidLength", false);
+                        return false;
+                    } else if(!uppercase_regex.test(viewValue)) {
+                        scope.resetPasswordValidity();
+                        ctrl.$setValidity("passwordInvalidUpper", false);
+                        return false;
+                    } else if(!lowercase_regex.test(viewValue)) {
+                        scope.resetPasswordValidity();
+                        ctrl.$setValidity("passwordInvalidLower", false);
+                        return false;
+                    } else if(!numeric_regex.test(viewValue)) {
+                        scope.resetPasswordValidity();
+                        ctrl.$setValidity("passwordInvalidNumeric", false);
+                        return false;
+                    } else if(!nonalpha_regex.test(viewValue)) {
+                        scope.resetPasswordValidity();
+                        ctrl.$setValidity("passwordInvalidNonalpha", false);
+                        return false;
+                    }
+                    scope.resetPasswordValidity();
+                    return true;
                 };
             }
         };
@@ -25,11 +69,14 @@ angular.module('lpValidateDirectives', [])
                 ctrl.$asyncValidators.username = function (modelValue, viewValue) {
                     return $q(function (resolve, reject) {
                         UserDetail.checkValidity(viewValue).then(function(data) {
-                            console.log("data: " + data.data);
-                            if(data.data === null)
+                            if(data.data === null) {
+                                ctrl.$setValidity("usernameExists", true);
                                 resolve();
-                            else
+                            }
+                            else {
+                                ctrl.$setValidity("usernameExists", false);
                                 reject(data.data.errorMessage);
+                            }
                         });
                     });
                 };
@@ -45,8 +92,10 @@ angular.module('lpValidateDirectives', [])
             link: function(scope, elm, attrs, ctrl) {
                 ctrl.$validators.lpValidateEmail = function(modelValue, viewValue) {
                     if(email_regex.test(viewValue)) {
+                        ctrl.$setValidity("emailInvalidFormat", true);
                         return true;
                     }
+                    ctrl.$setValidity("emailInvalidFormat", false);
                     return false;
                 };
             }
@@ -61,8 +110,10 @@ angular.module('lpValidateDirectives', [])
             link: function(scope, elm, attrs, ctrl) {
                 ctrl.$validators.lpValidateName = function(modelValue, viewValue) {
                     if(name_regex.test(viewValue)) {
+                        ctrl.$setValidity("nameInvalidFormat", true);
                         return true;
                     }
+                    ctrl.$setValidity("nameInvalidFormat", false);
                     return false;
                 };
             }
